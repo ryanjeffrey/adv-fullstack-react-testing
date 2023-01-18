0c7557880351e0c560bdb75d716c93561b85b127
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-01-17 20:03:19 -0600

    Create form component

--- src/components/ShoppingList/ShoppingListItemForm.jsx
+++ src/components/ShoppingList/ShoppingListItemForm.jsx
@@ -1,3 +1,50 @@
-export default function ShoppingListItemForm() {
-  return <></>;
+import { useState } from 'react';
+
+const defaultShoppingListItem = {
+  id: null,
+  item_name: '',
+  quantity: '',
+};
+
+export default function ShoppingListItemForm({
+  id,
+  shoppingListItem,
+  onSubmit,
+}) {
+  const [currentShoppingListItem, setCurrentShoppingListItem] =
+    useState(shoppingListItem || defaultShoppingListItem);
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit(currentShoppingListItem);
+        console.log(currentShoppingListItem);
+        setCurrentShoppingListItem(defaultShoppingListItem);
+      }}
+    >
+      <input
+        type="text"
+        placeholder="item"
+        value={currentShoppingListItem.item_name}
+        onChange={(e) =>
+          setCurrentShoppingListItem({
+            ...currentShoppingListItem,
+            item_name: e.target.value,
+          })
+        }
+      />
+      <input
+        type="number"
+        placeholder="qty"
+        value={currentShoppingListItem.quantity}
+        onChange={(e) =>
+          setCurrentShoppingListItem({
+            ...currentShoppingListItem,
+            quantity: e.target.value,
+          })
+        }
+      />
+      <button type="submit">Add Item</button>
+    </form>
+  );
 }
