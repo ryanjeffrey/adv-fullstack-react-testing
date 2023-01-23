d8793faa024fd67330f1c9d90b56651e16942449
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-01-23 16:52:36 -0600

    Add test that checks if submit event submits the shoppingList

--- src/components/ShoppingList/ShoppingListForm.test.jsx
+++ src/components/ShoppingList/ShoppingListForm.test.jsx
@@ -1,9 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import ShoppingListForm from './ShoppingListForm.jsx';
 
 describe('ShoppingListForm', () => {
   it('has a form with a submit button', () => {
     render(<ShoppingListForm />);
     expect(screen.queryByText('submit'));
+  });
+
+  it('submits the given shoppingList', () => {
+    const shoppingList = {
+      name: 'sporting goods',
+      shoppingItems: [],
+    };
+    const onSubmit = jest.fn();
+    render(
+      <ShoppingListForm
+        id={'new'}
+        shoppingList={shoppingList}
+        onSubmit={onSubmit}
+      />
+    );
+    fireEvent.click(
+      screen.getByTestId('shopping-list-form-submit-button-new')
+    );
+    expect(onSubmit).toHaveBeenCalledWith(shoppingList);
   });
 });
