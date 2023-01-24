3fa3ede706416cf3d02b97df67fcca4fcebc1b58
ryanjeffrey <ryanjeffreysmith@gmail.com>
2023-01-23 18:20:41 -0600

    Add test for onSubmit function

--- src/components/ShoppingList/ShoppingListItemForm.test.jsx
+++ src/components/ShoppingList/ShoppingListItemForm.test.jsx
@@ -1,9 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import ShoppingListItemForm from './ShoppingListItemForm.jsx';
 
 describe('ShoppingListItemForm', () => {
   it('has a form with a submit button', () => {
     render(<ShoppingListItemForm />);
     expect(screen.queryByText('submit'));
+  });
+
+  it('submits the given shoppingList', () => {
+    const shoppingListItem = {
+      name: 'tennis balls',
+      quantity: 14
+    };
+    const onSubmit = jest.fn();
+    render(
+      <ShoppingListItemForm
+        id={'new'}
+        shoppingListItem={shoppingListItem}
+        onSubmit={onSubmit}
+      />
+    );
+    fireEvent.click(
+      screen.getByTestId('shopping-list-item-form-submit-button-new')
+    );
+    expect(onSubmit).toHaveBeenCalledWith(shoppingListItem);
   });
 });
